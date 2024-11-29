48fe50978d51a6b0a78736b05f7e06f21c237499
Pavitra <pavitra@gmail.com>
2024-11-29 22:41:15 +0530

    edit and delete route created

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const PORT = 8000;
-const Listing = require("./models/listing")
-const path = require("path")
+const Listing = require("./models/listing");
+const path = require("path");
+const methodOverride = require("method-override")
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/backend_project_01"
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/backend_project_01";
 main()
-.then(() =>{
-    console.log("Mongodb connected successfully")
-})
-.catch(() =>{
-    console.log("mongodb connection error")
-})
-async function main(){
-   await mongoose.connect(MONGO_URL)
+  .then(() => {
+    console.log("Mongodb connected successfully");
+  })
+  .catch(() => {
+    console.log("mongodb connection error");
+  });
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended:true}));
-
-
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"))
 // app.get("/listing", async(req,res) =>{
 //     const sampleListing = new Listing({
 //         title:"My new Villa",
@@ -36,27 +37,70 @@ app.use(express.urlencoded({extended:true}));
 //     res.send("successful testing")
 // })
 
-app.get("/listings", async(req, res) =>{
-    const allListing = await Listing.find()
-    // res.send(listing)
-    res.render("listings/index.ejs",{allListing})
-})
+//Create Route
+app.post("/listings", async (req, res) => {
+  try{
+    console.log("Incoming data:", req.body.listing);
+    const newListing = new Listing(req.body.listing);
+    console.log("Listing created:", newListing)
 
-app.get("/listings/:id", async (req, res) => {
-    const { id } = req.params; 
-    try {
-        const listing = await Listing.findById(id);
-        if (!listing) {
-            return res.status(404).send("Listing not found");
-        }
-        res.render("listings/show", { listing }); 
-    } catch (error) {
-        console.error("Error fetching listing:", error); 
-        res.status(500).send("Internal Server Error"); 
-    }
+    await newListing.save();
+    console.log("Listing Saved");
+    res.redirect("/listings")
+  }catch(error){
+    console.error("error in creating new list")
+  }
 });
 
 
-app.listen(PORT, () =>{
-    console.log(`Server is running at PORT ${PORT}`)
+app.get("/listings", async (req, res) => {
+  try {
+    const allListing = await Listing.find();
+    console.log("Fetching listings: ", allListing)
+    res.render("listings/index.ejs", { allListing });
+  } catch (error) {
+    console.error("Error fetching listings:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+
+
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+
+//edit route
+app.get("/listings/:id/edit", async(req,res) =>{
+  const {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", {listing});
 })
+
+//updated route
+app.put("/listings/:id", async(req, res) =>{
+  let {id} = req.params;
+  await Listing.findByIdAndUpdate(id, {...req.body.listing})
+  res.redirect("/listings");
+})
+
+//delete route
+app.delete("/listings/:id", async(req,res) =>{
+  let {id} = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings")
+}) 
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT ${PORT}`);
+});
